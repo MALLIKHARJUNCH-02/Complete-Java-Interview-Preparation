db29d69fdbe8e98e731bf720b7004be005811b6f
MALLIKHARJUNCH-02 <2k2cmk@gmail.com>
2026-07-15 13:47:16 +0530

    Updated Jdbc

--- JDBC/CRUD.java
+++ JDBC/CRUD.java
@@ -14,9 +14,10 @@ public class CRUD {
             sc = new Scanner(System.in);
 
             System.out.println("Enter The Operation You Want to Perform On the Database");
-            System.out.println("Insert - ( 1 )");
-            System.out.println("Update - ( 2 )");
-            System.out.println("Delete - ( 3 )");
+            System.out.println("Insert The Record -> Choice [ 1 ]");
+            System.out.println("Update The Record -> Choice [ 2 ]");
+            System.out.println("Delete the Record -> Choice [ 3 ]");
+            System.out.println("Choice -> ");
             int choice = sc.nextInt();
 
             Driver d = new oracle.jdbc.OracleDriver();
@@ -31,17 +32,17 @@ public class CRUD {
             int n = 0;
             switch (choice) {
                 case 1:
-                    System.out.println("Enter the Employee Id");
+                    System.out.print("Enter the Employee Id : ");
                     int emp_id = sc.nextInt();
 
                     sc.nextLine();
-                    System.out.println("Enter The Employee Name");
+                    System.out.print("Enter The Employee Name : ");
                     String emp_name = sc.nextLine();
 
-                    System.out.println("Enter The USer Department : ");
+                    System.out.print("Enter The USer Department : ");
                     String department = sc.nextLine();
 
-                    System.out.println("Enter The Salary");
+                    System.out.print("Enter The Salary : ");
                     double salary = sc.nextDouble();
 
                     // System.out.print("Enter Hire Date (yyyy-MM-dd) ex: (15-JAN-24): ");
@@ -50,7 +51,7 @@ public class CRUD {
                     // Date hire_date = Date.valueOf(inputDate);
 
                     sc.nextLine();
-                    System.out.println("Enter The Employee City");
+                    System.out.print("Enter The Employee City : ");
                     String city = sc.nextLine();
 
                     n = stmt.executeUpdate(
@@ -58,28 +59,29 @@ public class CRUD {
                                     + "'," + salary + ",'14-JUN-2','" + city + "')");
 
                     if (n > 0) {
-                        System.out.println("Inserted The Record");
+                        System.out.print("Inserted The Record : ");
                     } else {
-                        System.out.println("Record is Not Inserted");
+                        System.out.print("Record is Not Inserted : ");
                     }
                     break;
                 case 2:
                     System.out.println("Enter What do You Want TO Update");
-                    System.out.println("Employee Id ( 1 )");
-                    System.out.println("Employee Name ( 2 )");
-                    System.out.println("Employee Department ( 3 )");
-                    System.out.println("Employee Salary ( 4 )");
+                    System.out.println("Update Employee Id -> Choice ( 1 )");
+                    System.out.println("Update Employee Name -> Choice ( 2 )");
+                    System.out.println("Update Employee Department -> Choice ( 3 )");
+                    System.out.println("Update Employee Salary -> Choice( 4 )");
                     // System.out.println("Employee Hire Date ( 5 )");
-                    System.out.println("Employee City ( 6 )");
+                    System.out.println("Update Employee City -> Choice ( 6 )");
+                    System.out.print("Choice Option -> ");
                     int updateChoice = sc.nextInt();
 
-                    System.out.println("Enter The Employee Id");
+                    System.out.print("Enter The Employee Id That You Want to Update");
                     int updateEmployeeId = sc.nextInt();
                     sc.nextLine();
 
                     switch (updateChoice) {
                         case 1:
-                            System.out.println("Enter The New Employee Id : ");
+                            System.out.print("Enter The New Employee Id : ");
                             int newEmployeeId = sc.nextInt();
 
                             n = stmt.executeUpdate("UPDATE employees set emp_id = " + newEmployeeId + " where emp_id = "
@@ -92,7 +94,7 @@ public class CRUD {
                             }
                             break;
                         case 2:
-                            System.out.println("Enter The New Employee Name: ");
+                            System.out.print("Enter The New Employee Name: ");
                             String newEmployeeName = sc.nextLine();
 
                             n = stmt.executeUpdate("UPDATE employees set emp_name = '" + newEmployeeName
@@ -106,7 +108,7 @@ public class CRUD {
                             break;
 
                         case 3:
-                            System.out.println("Enter The New Employee Department : ");
+                            System.out.print("Enter The New Employee Department : ");
                             String newEmployeeDepartment = sc.nextLine();
 
                             n = stmt.executeUpdate("UPDATE employees set department = '" + newEmployeeDepartment
@@ -120,7 +122,7 @@ public class CRUD {
                             break;
 
                         case 4:
-                            System.out.println("Enter The New Employee Salary : ");
+                            System.out.print("Enter The New Employee Salary : ");
                             double newEmployeeSalary = sc.nextDouble();
 
                             n = stmt.executeUpdate("UPDATE employees set salary = " + newEmployeeSalary
@@ -145,7 +147,7 @@ public class CRUD {
                         // break;
 
                         case 6:
-                            System.out.println("Enter The New Employee City : ");
+                            System.out.print("Enter The New Employee City : ");
                             String newEmployeeCity = sc.nextLine();
 
                             n = stmt.executeUpdate("UPDATE employees set city = '" + newEmployeeCity
@@ -165,7 +167,7 @@ public class CRUD {
                     break;
 
                 case 3:
-                    System.out.println("Enter the Employee Id That Wanted to Delete");
+                    System.out.print("Enter the Employee Id That Wanted to Delete : ");
                     int deleteEmployeeId = sc.nextInt();
 
                     n = stmt.executeUpdate("delete from employees where emp_id = " + deleteEmployeeId);
